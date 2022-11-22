78bca21ecd461d73063726982c35d8fb92e6b8b4
Mario Ruano <mrf1989@hotmail.com>
2022-11-22 17:10:27 +0100

    Remove root endpoint no used

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const songsRouter = require('./routes/songs');
-const likesRouter = require('./routes/likes');
+const songsRouter = require("./routes/songs");
+const likesRouter = require("./routes/likes");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api/v1/songs', songsRouter);
-app.use('/api/v1/likes', likesRouter);
+app.use("/api/v1/songs", songsRouter);
+app.use("/api/v1/likes", likesRouter);
 
 module.exports = app;
