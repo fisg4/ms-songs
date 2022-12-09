a002b29fbf17d7d7be90fa69f6c932769df5d101
Mario Ruano <mrf1989@hotmail.com>
2022-12-09 18:23:41 +0100

    Fix problem with  query. MongoDB Atlas does not support that

--- routes/songs.js
+++ routes/songs.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const debug = require('debug')('ms-songs:server');
+const debug = require("debug")("ms-songs:server");
 
 const router = express.Router();
 
@@ -12,18 +12,16 @@ const spotifyService = require("../services/spotify");
 router.get("/", async function (req, res, next) {
   try {
     if (Object.keys(req.query).length === 0) {
-      const result = await Song.find()
-        .populate('likes', {
-          user: 1,
-          date: 1
-        }
-      );
+      const result = await Song.find().populate("likes", {
+        user: 1,
+        date: 1,
+      });
       if (result) res.send(result);
       else res.sendStatus(204);
     } else if (req.query.hasOwnProperty("title")) {
       next();
     }
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
@@ -32,18 +30,16 @@ router.get("/:id", async function (req, res, next) {
   try {
     const id = req.params.id;
     if (Object.keys(req.query).length === 0) {
-      const result = await Song.findById(id)
-        .populate('likes', {
-          user: 1,
-          date: 1
-        }
-      );
+      const result = await Song.findById(id).populate("likes", {
+        user: 1,
+        date: 1,
+      });
       if (result) res.send(result);
       else res.sendStatus(204);
     } else {
       next();
     }
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
@@ -53,18 +49,17 @@ router.get("/", async function (req, res, next) {
   try {
     if (Object.keys(req.query).length > 0) {
       const title = req.query.title.toLocaleLowerCase().trim();
-      const result = await Song.$where('this.title.toLocaleLowerCase().includes("'+ title + '")')
-        .populate('likes', {
-          user: 1,
-          date: 1
-        }
-      );
+      const regexTitle = new RegExp(`\\b${title}\\b`, "i");
+      const result = await Song.find({ title: regexTitle }).populate("likes", {
+        user: 1,
+        date: 1,
+      });
       if (result) res.send(result);
       else res.sendStatus(204);
     } else {
       next();
     }
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
@@ -73,12 +68,11 @@ router.get("/spotify", async function (req, res, next) {
   try {
     const title = req.query.title.toLocaleLowerCase().trim();
     let artist = "";
-    if (req.query.hasOwnProperty("artist"))
-      artist = req.query.artist.trim();
+    if (req.query.hasOwnProperty("artist")) artist = req.query.artist.trim();
 
     const response = await spotifyService.searchSongs(title, artist);
     if (response.status) {
-      const result = {songs: []};
+      const result = { songs: [] };
       if (response.songs.tracks) {
         for (track of response.songs.tracks.items) {
           let newSong = {
@@ -87,29 +81,29 @@ router.get("/spotify", async function (req, res, next) {
             albumCover: track.album.images[0].url,
             releaseDate: track.album.release_date,
             url: track.preview_url,
-            spotifyId: track.id
-          }
-          track.artists.forEach(artist => {
-            newSong.artists.push(artist.name)
+            spotifyId: track.id,
+          };
+          track.artists.forEach((artist) => {
+            newSong.artists.push(artist.name);
           });
           result.songs.push(newSong);
         }
       }
-      console.log(response.status)
+      console.log(response.status);
       if (result.songs.length > 0) res.send(result);
       else res.sendStatus(204);
-
     } else {
       res.sendStatus(response.status);
     }
   } catch (err) {
     next(err);
   }
-  });
+});
 
 router.post("/", async function (req, res, next) {
   try {
-    const {title, artists, releaseDate, albumCover, url, lyrics, spotifyId} = req.body;
+    const { title, artists, releaseDate, albumCover, url, lyrics, spotifyId } =
+      req.body;
     const song = new Song({
       title,
       artists,
@@ -118,12 +112,12 @@ router.post("/", async function (req, res, next) {
       url,
       lyrics,
       spotifyId,
-      likes: []
+      likes: [],
     });
     await song.save();
     return res.sendStatus(201);
-  } catch(err) {
-    if (err?.code === 11000) res.status(409).send('Conflict: Duplicate');
+  } catch (err) {
+    if (err?.code === 11000) res.status(409).send("Conflict: Duplicate");
     else next(err);
   }
 });
@@ -144,30 +138,30 @@ router.post("/ticket", async function (req, res, next) {
     } else {
       res.sendStatus(400);
     }
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
 
 router.put("/", async function (req, res, next) {
   try {
-    const {id, url, lyrics} = req.body;
+    const { id, url, lyrics } = req.body;
     if (Object.keys(req.query).length === 0) {
       const newInfo = {};
       if (typeof url !== "undefined") newInfo.url = url;
       if (typeof lyrics !== "undefined") newInfo.lyrics = lyrics;
 
-      const result = await Song.findByIdAndUpdate(id, newInfo, {new: true})
-        .populate('likes', {
-          user: 1,
-          date: 1
-        }
-      );
+      const result = await Song.findByIdAndUpdate(id, newInfo, {
+        new: true,
+      }).populate("likes", {
+        user: 1,
+        date: 1,
+      });
       res.send(result);
     } else {
       res.sendStatus(400);
     }
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
@@ -176,9 +170,9 @@ router.delete("/:id", async function (req, res, next) {
   try {
     const id = req.params.id;
     const result = await Song.findByIdAndDelete(id);
-    const deletedLikes = await Like.deleteMany({song: id});
+    const deletedLikes = await Like.deleteMany({ song: id });
     res.sendStatus(204);
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
