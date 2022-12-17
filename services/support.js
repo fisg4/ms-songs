6d6ccbbbc8c786f3d09afcc873cba72284b68376
Mario Ruano <mrf1989@hotmail.com>
2022-12-17 13:43:50 +0100

    Integration with the support microservice for sending tickets

--- services/support.js
+++ services/support.js
@@ -1,9 +1,34 @@
-function postTicketToChangeVideoUrl({ id, url }) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(true);
-    }, 1500);
-  });
+const axios = require("axios");
+const urlJoin = require("url-join");
+
+async function postTicketToChangeVideoUrl({ authorId, songId, text }) {
+  const url = urlJoin(process.env.SUPPORT_HOST, "/support/v1/tickets");
+  const response = await axios.post(
+    url,
+    {
+      authorId,
+      songId,
+      text,
+      title: "Video clip change request",
+      priority: "medium",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  if (response.status === 201) {
+    return {
+      status: 201,
+      message: "Support ticket posted succesfully",
+      ticket: response.data,
+    };
+  } else {
+    throw Error(
+      `Failed to post support ticket, reported with status code: ${response.status}`
+    );
+  }
 }
 
 module.exports = {
